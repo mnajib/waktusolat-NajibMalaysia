7ba0511ba6af004e44930edb2c01607d6d1bbf7e
Najib Ibrahim <mnajib@gmail.com>
2026-08-16 08:14:15 +0800

    fix output format for xmobar. waktusolat-reminder: sleep 0.5

--- lib/formatters/xmobar.sh
+++ lib/formatters/xmobar.sh
@@ -24,10 +24,49 @@ fi
 #get_prayer_colors "$MGH" "$NOW_HM" "$TOGGLE" MGH_FG MGH_BG
 #get_prayer_colors "$ISY" "$NOW_HM" "$TOGGLE" ISY_FG ISY_BG
 
+
+#---------------------------------------
 # Write entirely to the isolated temp file first, using pre-calculated prayer colors
-cat <<EOF > "$TMP_OUT"
-<fc=#888888>Data ${SERVER_TIME};</fc>${OLD_TXT}<fc=#ff66ff>(${ZONE}</fc> <fc=#00ffff>(${G_MONTH_ABB}</fc> <fc=#00ffff>${G_DATE_ISO}</fc> <fc=#00ffff>${G_DAY_ABB}</fc> <fc=#ffff00>(${H_MONTH_NAME}</fc> <fc=#ffff00>${HDATE}</fc> <fc=#ffff00>${H_DAY_NAME}</fc> <fc=#000000,#ffffff>Fjr</fc><fc=#${FJR_FG},#${FJR_BG}> ${FJR} </fc> <fc=#000000,#ffffff>Syu</fc><fc=#${SYU_FG},#${SYU_BG}> ${SYU} </fc> <fc=#000000,#ffffff>Zhr</fc><fc=#${ZHR_FG},#${ZHR_BG}> ${ZHR} </fc> <fc=#000000,#ffffff>Asr</fc><fc=#${ASR_FG},#${ASR_BG}> ${ASR} </fc><fc=#ffff00>)</fc> <fc=#ffff00>(${H_NEXT_DAY_NAME}</fc> <fc=#000000,#ffffff>Mgh</fc><fc=#${MGH_FG},#${MGH_BG}> ${MGH} </fc> <fc=#000000,#ffffff>Isy</fc><fc=#${ISY_FG},#${ISY_BG}> ${ISY} </fc><fc=#ffff00>)</fc><fc=#00ffff>)</fc><fc=#ff66ff>)</fc>
-EOF
+#---------------------------------------
+
+#cat <<EOF > "$TMP_OUT"
+#<fc=#888888>Data ${SERVER_TIME};</fc>${OLD_TXT}<fc=#ff66ff>(${ZONE}</fc> <fc=#00ffff>(${G_MONTH_ABB}</fc> <fc=#00ffff>${G_DATE_ISO}</fc> <fc=#00ffff>${G_DAY_ABB}</fc> <fc=#ffff00>(${H_MONTH_NAME}</fc> <fc=#ffff00>${HDATE}</fc> <fc=#ffff00>${H_DAY_NAME}</fc> <fc=#000000,#ffffff>Fjr</fc><fc=#${FJR_FG},#${FJR_BG}> ${FJR} </fc> <fc=#000000,#ffffff>Syu</fc><fc=#${SYU_FG},#${SYU_BG}> ${SYU} </fc> <fc=#000000,#ffffff>Zhr</fc><fc=#${ZHR_FG},#${ZHR_BG}> ${ZHR} </fc> <fc=#000000,#ffffff>Asr</fc><fc=#${ASR_FG},#${ASR_BG}> ${ASR} </fc><fc=#ffff00>)</fc> <fc=#ffff00>(${H_NEXT_DAY_NAME}</fc> <fc=#000000,#ffffff>Mgh</fc><fc=#${MGH_FG},#${MGH_BG}> ${MGH} </fc> <fc=#000000,#ffffff>Isy</fc><fc=#${ISY_FG},#${ISY_BG}> ${ISY} </fc><fc=#ffff00>)</fc><fc=#00ffff>)</fc><fc=#ff66ff>)</fc>
+#EOF
+
+#cat <<EOF> "${TMP_OUT}"
+#<fc=#888888>Data ${SERVER_TIME};</fc>${OLD_TXT}\
+#<fc=#ff66ff>(${ZONE}</fc> \
+#<fc=#00ffff>(${G_MONTH_ABB} ${G_DATE_ISO} ${G_DAY_ABB}</fc> \
+#<fc=#ffff00>(${H_MONTH_NAME} ${HDATE} ${H_DAY_NAME}</fc> \
+#<fc=#000000,#ffffff>Fjr</fc><fc=#${FJR_FG},#${FJR_BG}> ${FJR} </fc> \
+#<fc=#000000,#ffffff>Syu</fc><fc=#${SYU_FG},#${SYU_BG}> ${SYU} </fc> \
+#<fc=#000000,#ffffff>Zhr</fc><fc=#${ZHR_FG},#${ZHR_BG}> ${ZHR} </fc> \
+#<fc=#000000,#ffffff>Asr</fc><fc=#${ASR_FG},#${ASR_BG}> ${ASR} </fc>\
+#<fc=#ffff00>)</fc> \
+#<fc=#ffff00>(${H_NEXT_DAY_NAME}</fc> \
+#<fc=#000000,#ffffff>Mgh</fc><fc=#${MGH_FG},#${MGH_BG}> ${MGH} </fc> \
+#<fc=#000000,#ffffff>Isy</fc><fc=#${ISY_FG},#${ISY_BG}> ${ISY} </fc>\
+#<fc=#ffff00>)</fc><fc=#00ffff>)</fc><fc=#ff66ff>)</fc>
+#EOF
+
+#cat <<EOF> "${TMP_OUT}"
+#<fc=#888888>Data ${SERVER_TIME};</fc>${OLD_TXT}<fc=#ff66ff>(${ZONE}</fc> <fc=#00ffff>(${G_MONTH_ABB} ${G_DATE_ISO} ${G_DAY_ABB}</fc> <fc=#ffff00>(${H_MONTH_NAME} ${HDATE} ${H_DAY_NAME}</fc> <fc=#000000,#ffffff>Fjr</fc><fc=#${FJR_FG},#${FJR_BG}> ${FJR} </fc> <fc=#000000,#ffffff>Syu</fc><fc=#${SYU_FG},#${SYU_BG}> ${SYU} </fc> <fc=#000000,#ffffff>Zhr</fc><fc=#${ZHR_FG},#${ZHR_BG}> ${ZHR} </fc> <fc=#000000,#ffffff>Asr</fc><fc=#${ASR_FG},#${ASR_BG}> ${ASR} </fc><fc=#ffff00>)</fc> <fc=#ffff00>(${H_NEXT_DAY_NAME}</fc> <fc=#000000,#ffffff>Mgh</fc><fc=#${MGH_FG},#${MGH_BG}> ${MGH} </fc> <fc=#000000,#ffffff>Isy</fc><fc=#${ISY_FG},#${ISY_BG}> ${ISY} </fc><fc=#ffff00>)</fc><fc=#00ffff>)</fc><fc=#ff66ff>)</fc>
+#EOF
+
+TMP_CONTENT="<fc=#888888>Data ${SERVER_TIME};</fc>${OLD_TXT}"
+TMP_CONTENT+="<fc=#ff66ff>(${ZONE}</fc> "
+TMP_CONTENT+="<fc=#00ffff>(${G_MONTH_ABB} ${G_DATE_ISO} ${G_DAY_ABB}</fc> "
+TMP_CONTENT+="<fc=#ffff00>(${H_MONTH_NAME} ${HDATE} ${H_DAY_NAME}</fc> "
+TMP_CONTENT+="<fc=#000000,#ffffff>Fjr</fc><fc=#${FJR_FG},#${FJR_BG}> ${FJR} </fc> "
+TMP_CONTENT+="<fc=#000000,#ffffff>Syu</fc><fc=#${SYU_FG},#${SYU_BG}> ${SYU} </fc> "
+TMP_CONTENT+="<fc=#000000,#ffffff>Zhr</fc><fc=#${ZHR_FG},#${ZHR_BG}> ${ZHR} </fc> "
+TMP_CONTENT+="<fc=#000000,#ffffff>Asr</fc><fc=#${ASR_FG},#${ASR_BG}> ${ASR} </fc><fc=#ffff00>)</fc> "
+TMP_CONTENT+="<fc=#ffff00>(${H_NEXT_DAY_NAME}</fc> "
+TMP_CONTENT+="<fc=#000000,#ffffff>Mgh</fc><fc=#${MGH_FG},#${MGH_BG}> ${MGH} </fc> "
+TMP_CONTENT+="<fc=#000000,#ffffff>Isy</fc><fc=#${ISY_FG},#${ISY_BG}> ${ISY} </fc><fc=#ffff00>)</fc><fc=#00ffff>)</fc><fc=#ff66ff>)</fc>"
+echo -n "$TMP_CONTENT" > "$TMP_OUT"
+#---------------------------------------
+
 
 # Instantly swap the temp file into place atomically
 mv -f "$TMP_OUT" "$FINAL_OUT"
